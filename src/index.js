d366ab545d266df93ed890886b7b417f47792173
anupamagaur16 <anupamagaur16@gmail.com>
2021-06-27 13:36:32 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <span>
+      Image Viewer
+  </span>, 
+  document.getElementById('root')
+);
