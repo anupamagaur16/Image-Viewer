bf1d4429be408edd0d588436c8d6e142fa9687ea
anupamagaur16 <anupamagaur16@gmail.com>
2021-06-27 13:57:21 +0530

    Installed Material UI. Redirected index to login page

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './Login.css';
+import Header from '../../common/Header';
 
 class Login extends Component {
 	render(){
 		return(
         <div>
-            <h1>React with Instagram API</h1>
+            <Header heading="Image Viewer" />
         </div>)
 	}
 }
